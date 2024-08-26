ea56a3739cc6c82d8500b2706951faec1e24ddbb
李响 <491231391@qq.com>
2024-08-26 15:05:23 +0800

    lx

---
+++ server/app.js
@@ -0,0 +1,23 @@
+// 导入express，全局挂载
+import express from 'express'
+const app = express()
+// 导入cors，解决跨域问题，全局挂载
+import cors from 'cors'
+app.use(cors())
+// 导入bodyParser，解析请求体，全局挂载
+import bodyParser from 'body-parser'
+// application/josn
+app.use(bodyParser.json())
+// application/x-www-form-urlencoded
+// 当extended为false时，只会解析字符串或数组，true时，可以解析任意类型
+app.use(bodyParser.urlencoded({ extended: false }))
+// 设置默认端口
+const port = 3000
+// 监听端口
+app.listen(port, () => {
+    console.log(`running on port http://localhost:${port}`);
+})
+// get请求，返回hello lx!
+app.get('/', (req, res) => {
+    res.send('Hello lx!')
+})
